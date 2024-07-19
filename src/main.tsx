3721aa08451e7d0e0f7eb806916b5fdd9badbc64
Chris <cbega1227@gmail.com>
2024-07-18 19:49:18 -0600

    Installed styled-components dependency and wrapped the app with the theme provider.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/reset.styles.css";
-
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
