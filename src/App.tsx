386553f9509bed7b3df4452a1fedab1646bf8009
Chris B <cbega1227@gmail.com>
2024-07-18 21:57:02 -0600

    Created new fonts directory and uploaded the new custom font file. Created new fontFace in global.styles.ts using new custom font and included this in the theme as a fontFamilyHeader. Created new header component and created h1-h6 styled components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { H1, H2, H3, H4, H5, H6 } from "./components/Headings/headings";
 import React from "react";
 
 const App: React.FC = () => {
   return (
     <>
-      <p>hello world</p>
+      <H1>Hello World</H1>
+      <H2>Hello world</H2>
+      <H3>Hello world</H3>
+      <H4>Hello world</H4>
+      <H5>Hello world</H5>
+      <H6>Hello world</H6>
     </>
   );
 };
