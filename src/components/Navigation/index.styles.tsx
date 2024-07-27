a2ae8f838953354207a29768b116373954eec6f0
Chris <cbega1227@gmail.com>
2024-07-27 09:59:14 -0600

    Installed framer motion and started working on creating a animation for the menu items. Still need to clean up code but got a working menu.

--- src/components/Navigation/index.styles.tsx
+++ src/components/Navigation/index.styles.tsx
@@ -1,21 +1,43 @@
 import styled from "styled-components";
 // import { theme } from "../../styles/theme";
+import { motion } from "framer-motion";
 
 export const StyledNavigationWrapper = styled.div`
   position: sticky;
   top: 0;
-
   padding: 1rem;
 `;
 
-export const StyledNavigation = styled.nav`
+export const StyledNavigation = styled(motion.nav)`
   display: flex;
   justify-content: space-between;
-  align-items: center;
   padding: 16px 20px;
   background-color: rgba(255, 255, 255, 0.1);
   backdrop-filter: blur(20px);
   -webkit-backdrop-filter: blur(20px);
-  border-radius: 100px;
   border: 1px solid rgba(255, 255, 255, 0.2);
+`;
+
+export const StyledNavigationContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  align-items: center;
+  width: 100%;
+`;
+
+export const StyledNavigationBar = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+// move to nav list component
+
+export const StyledNavigationMenuList = styled(motion.ul)`
+  color: #fff;
+`;
+export const StyledNavigationMenuListItem = styled(motion.li)`
+  color: #fff;
 `;
--- src/components/Navigation/navigation.tsx
+++ src/components/Navigation/navigation.tsx
@@ -1,16 +1,89 @@
+// when i click the height animates
+// but because of the styles on the styledNavigation with border-radius 100px (to make the original navbar a pill shape) when the height changes the border-radius doesn't look appealing and is animating.....
+// how can i make this navigation more seemless and not so unappealing....
+
+// When the user clicks on the menu icon --> menu icon changes to an close icon
+// When the user clicks on the menu icon --> display the menu list
+
+// keep the navigation visible with a transparent background
+// when the user click to open/close animate a ::before pseudo element or animate another div
+
+import React, { useState } from "react";
 import Icon from "../../components/Icon/icon";
-import React from "react";
-import { StyledNavigation, StyledNavigationWrapper } from "./index.styles.tsx";
 import Typography from "../../components/Typography/typography.tsx";
+import {
+  StyledNavigation,
+  StyledNavigationBar,
+  StyledNavigationContainer,
+  StyledNavigationMenuList,
+  StyledNavigationMenuListItem,
+  StyledNavigationWrapper,
+} from "./index.styles.tsx";
+
+const navigationVariants = {
+  closed: {
+    height: "60px",
+    borderRadius: "100px",
+    transition: {
+      ease: "easeInOut",
+      duration: 0.5,
+      delay: 0.5,
+    },
+  },
+  open: {
+    borderRadius: "24px",
+    height: "304px",
+    transition: {
+      ease: "easeInOut",
+      duration: 0.5,
+    },
+  },
+};
+
+const navigationLinkVariants = {
+  closed: {
+    opacity: 0,
+    y: -20,
+    transition: {
+      duration: 0.6,
+      staggerChildren: 0.3,
+    },
+  },
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      staggerChildren: 0.3,
+    },
+  },
+};
+
 const Navigation: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <StyledNavigationWrapper>
-      <StyledNavigation>
-        {/* <span style={{ backgroundColor: "red", position: "absolute", zIndex: -1, right: "50%" }}>aafafaf</span> */}
-        <Typography component="p" variant="navigation_text">
-          Christian Bega
-        </Typography>
-        <Icon src="menu.svg" alt="Closed Menu Icon" size="32px"></Icon>
+    <StyledNavigationWrapper id="navigation-wrapper">
+      <StyledNavigation id="navigation" initial="closed" animate={isOpen ? "open" : "closed"} variants={navigationVariants}>
+        <StyledNavigationContainer id="navigation-container">
+          <StyledNavigationBar id="navigation-bar">
+            <Typography component="p" variant="navigation_text">
+              Christian Bega
+            </Typography>
+            <Icon onClick={toggleDropdown} src={isOpen ? "close-icon.svg" : "menu.svg"} alt="Navigation menu icon" size="32px" />
+          </StyledNavigationBar>
+          {/* move code to nav list component */}
+          <StyledNavigationMenuList initial="closed" animate={isOpen ? "open" : "closed"} variants={navigationLinkVariants}>
+            <StyledNavigationMenuListItem variants={navigationLinkVariants}>1</StyledNavigationMenuListItem>
+            <StyledNavigationMenuListItem variants={navigationLinkVariants}>2</StyledNavigationMenuListItem>
+            <StyledNavigationMenuListItem variants={navigationLinkVariants}>3</StyledNavigationMenuListItem>
+            <StyledNavigationMenuListItem variants={navigationLinkVariants}>4</StyledNavigationMenuListItem>
+          </StyledNavigationMenuList>
+        </StyledNavigationContainer>
       </StyledNavigation>
     </StyledNavigationWrapper>
   );
