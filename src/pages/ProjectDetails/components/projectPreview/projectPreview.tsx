abb64a393bd90a5326f9e51fe0780f541432421a
Chris <cbega1227@gmail.com>
2024-08-24 06:53:36 -0600

    Fixed issue with videoUrl not matching the expected type and was failing deployment. Also rename the ProjectImage component to ProjectVideo.

--- src/pages/ProjectDetails/components/projectPreview/projectPreview.tsx
+++ src/pages/ProjectDetails/components/projectPreview/projectPreview.tsx
@@ -4,7 +4,7 @@ import Typography from "../../../../components/Typography/typography";
 import TextBlock from "../../../../components/TextBlock/textBlock";
 import Icon from "../../../../components/Icon/icon";
 import { textBlockProjectPreviewStylesDesktop, textBlockProjectPreviewStylesMobile } from "../../projectDetails.styles";
-import ProjectImage from "../../../../components/ProjectVideo/projectVideo";
+import ProjectVideo from "../../../../components/ProjectVideo/projectVideo";
 import GridContainer from "../../../../components/GridContainer/gridContainer";
 import GridItem from "../../../../components/GridItem/gridItem";
 import useMediaQueries from "../../../../utils/useMediaQueries/useMediaQueries";
@@ -14,7 +14,7 @@ const ProjectPreview: React.FC<ProjectPreviewProps> = ({ title, description, vid
   return (
     <GridContainer id="project-preview-section" mb={isSm ? "0" : "92px"}>
       <GridItem paddingX="0px" id="project-image-section" order={isSm ? 1 : 0}>
-        <ProjectImage type="video" videoUrl={videoUrl} />
+        <ProjectVideo videoUrl={{ desktop: videoUrl.desktop, mobile: videoUrl.mobile }} />
       </GridItem>
       <GridItem paddingX="0px" id="project-content-section" order={isSm ? 0 : 1}>
         <TextBlock containerStyles={isSm ? textBlockProjectPreviewStylesDesktop : textBlockProjectPreviewStylesMobile}>
