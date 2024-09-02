4a59433552d772b9bfc7a5e15e625318c0de95ca
Chris <cbega1227@gmail.com>
2024-09-02 07:20:02 -0600

    Updated footer urls to include links to github, linkedin, etc.

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -4,7 +4,7 @@ import Typography from "../../components/Typography/typography";
 import { StyledEclipseBg, StyledEclipseBgOrb, StyledFooter, StyledFooterWrapper } from "./footer.styles";
 import useMediaQueries from "../../utils/useMediaQueries/useMediaQueries";
 import { MediaQueries } from "../../utils/useMediaQueries/types";
-
+// import { HashLink } from "react-router-hash-link";
 const listItemStyle = {
   display: "flex",
   alignItems: "center",
@@ -12,6 +12,7 @@ const listItemStyle = {
 
 const Footer: React.FC<MediaQueries> = () => {
   const { isSm, isMd } = useMediaQueries();
+
   return (
     <StyledFooterWrapper>
       <StyledEclipseBgOrb isSm={isSm} isMd={isMd} />
@@ -23,13 +24,19 @@ const Footer: React.FC<MediaQueries> = () => {
           </Typography>
           <ul className="footer-content-children" role="list" style={{ display: "flex", gap: "2rem" }}>
             <li style={{ ...listItemStyle }}>
-              <Icon src="/icons/Mail-Icon.svg" size="32px" alt="Github Icon"></Icon>
+              <a target="_blank" rel="noopener noreferrer" href="mailto:cbega1227@gmail.com" aria-label="Send an email to me!">
+                <Icon src="/icons/Mail-Icon.svg" size="32px" alt="Github Icon"></Icon>
+              </a>
             </li>
             <li style={{ ...listItemStyle }}>
-              <Icon src="/icons/LinkedIn-Icon.svg" size="32px" alt="Github Icon"></Icon>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/christian-bega-4b63b3216/">
+                <Icon src="/icons/LinkedIn-Icon.svg" size="32px" alt="Github Icon"></Icon>
+              </a>
             </li>
             <li style={{ ...listItemStyle }}>
-              <Icon src="/icons/github-icon.svg" size="28px" alt="Github Icon"></Icon>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/ChristianBega">
+                <Icon src="/icons/github-icon.svg" size="28px" alt="Github Icon"></Icon>
+              </a>
             </li>
           </ul>
         </div>
